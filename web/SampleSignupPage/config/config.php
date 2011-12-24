3004417b24a9aa04e80d6e73538964fe54a3b36d
sanctum32 <andriuspel@gmail.com>
2011-12-25 01:04:37 +0200

    web folder is going to be added to downloads

--- web/SampleSignupPage/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conf["host"] = "127.0.0.1";	//Mysql host
-$conf["user"] = "user";			//Mysql username
-$conf["password"] = "password";	//Mysql password
-$conf["db"] = "arcemu_logon";	//Arcemu logon database
-$conf["ok"] = "Your account has been created successfuly !";
-$conf["multi"] = true; // if you allow multi account its true else false (default not allowed)
-$conf["url"] = "#";	//server main url
-?>
