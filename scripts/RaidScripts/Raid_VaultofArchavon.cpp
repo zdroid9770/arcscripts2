81e06233f1994fd64854049f1e328faa6b298249
sanctum32 <andriuspel@gmail.com>
2012-01-14 22:39:58 +0200

    more changes on boss archavon

--- scripts/RaidScripts/Raid_VaultofArchavon.cpp
+++ scripts/RaidScripts/Raid_VaultofArchavon.cpp
@@ -18,15 +18,14 @@
  
 #include "Setup.h"
 
-#define	NPC_ARCHAVON	31125
-
 //Archavon
-#define SPELL_ROCK_SHARDS	58678
-#define	SPELL_CRUSHING_LEAP	Raid25manInst(58960, 60894)
-#define	SPELL_STOMP			Raid25manInst(58663, 60880)
-#define SPELL_IMPALE		Raid25manInst(58666, 60882)
+#define	NPC_ARCHAVON			31125
+#define SPELL_ROCK_SHARDS_LEFT	58678
+#define	SPELL_CRUSHING_LEAP		Raid25manInst(58963, 60895)
+#define	SPELL_STOMP				Raid25manInst(58663, 60880)
+#define SPELL_IMPALE			Raid25manInst(58666, 60882)
 #define	SPELL_CHOCKING_CLOUD	Raid25manInst(58965, 61672)
-#define	SPELL_ARCHAVON_BERSERK		47008
+#define	SPELL_ARCHAVON_BERSERK	47008
 
 class ArchavonAI : public MoonScriptBossAI
 {
@@ -34,37 +33,36 @@ class ArchavonAI : public MoonScriptBossAI
 		MOONSCRIPT_FACTORY_FUNCTION(ArchavonAI, MoonScriptBossAI);
 		ArchavonAI(Creature *pCreature) : MoonScriptBossAI(pCreature)
 		{
-			mStompTimer = -1;
+			mStompTimer = mRockShardsTimer = INVALIDATE_TIMER;
 			SetEnrageInfo(AddSpell(SPELL_ARCHAVON_BERSERK, Target_Self, 0.0f), MINUTE*5*SEC_IN_MS);
+		
 			AddSpell(SPELL_ROCK_SHARDS, Target_Self, 20.0f);
 			AddSpell(SPELL_CRUSHING_LEAP, Target_RandomPlayer, 30.0f);
 			AddSpell(SPELL_CHOCKING_CLOUD, Target_RandomPlayer, 30.0f);
-
-			mStoned = AddSpell(SPELL_IMPALE, Target_Self, 30.0f);
-			mStomp = AddSpell(SPELL_STOMP, Target_RandomPlayer, 30.0f);
 		}
 
 		void OnCombatStart(Unit *mTarget)
 		{
 			mStompTimer = AddTimer(45*SEC_IN_MS);
+			mRockShardsTimer = AddTimer(15*SEC_IN_MS);
 			ParentClass::OnCombatStart(mTarget);
 		}
 
-
 		void AIUpdate()
 		{
-			if(IsTimerFinished(iStompTimer))
+			if(IsTimerFinished(mStompTimer))
 			{
-				CastSpell(mStomp);
-				CastSpell(mStoned);
-				ResetTimer(iStompTimer, 45*SEC_IN_MS);
+				_unit->CastSpell(_unit, SPELL_STOMP, false);
+				_unit->CastSpell(_unit, SPELL_IMPALE_STUN, false);
+				_unit->CastSpell(_unit, SPELL_IMPALE, false);
+				ResetTimer(mStompTimer, 45*SEC_IN_MS);
 			}
 			ParentClass::AIUpdate();
 		}
 
 	private:
-		int32 mStompTimer;
-		SpellDesc *mStoned, *mStomp;
+		SpellDesc * sRockShardsLeft, *sRockShardsRight;
+		int32 mStompTimer, mRockShardsTimer;
 };
 
 void SetupVaultofArchavon(ScriptMgr * mgr)
