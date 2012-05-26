05bc5acae030cdb32dcc95fabeb551fb4692bc72
unknown <Ed@Ed-PC.(none)>
2012-05-25 23:17:22 -0400

    Fixed a compiling error.

--- scripts/InstanceScripts/Instance_UtgardeKeep.cpp
+++ scripts/InstanceScripts/Instance_UtgardeKeep.cpp
@@ -537,7 +537,7 @@ class SkeletonAddAI : public MoonScriptCreatureAI
 //////////////////////////////////////////////////////////////////////////////////////////
 #define CN_INGVAR_UNDEAD 23980
 #define CN_SHADOW_AXE 23996
-#define NPC_ANNHYLDE_SPAWN 24068
+//#define NPC_ANNHYLDE_SPAWN 24068
 
 // Phase 1 spells (Human Form)
 #define INGVAR_CLEAVE 42724
