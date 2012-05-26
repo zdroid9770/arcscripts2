a6e4845aa13be6b5c5d12051d354f24d530b9923
unknown <Ed@Ed-PC.(none)>
2012-05-25 21:01:48 -0400

    Trash clean-up and started on Forge of Souls.

--- scripts/InstanceScripts/Instance_ForgeOfSouls.cpp
+++ scripts/InstanceScripts/Instance_ForgeOfSouls.cpp
@@ -20,5 +20,53 @@
  
  void SetupForgeOfSouls(ScriptMgr * mgr)
  {
+
+//TODO: Script Devourer of Souls
+// In 3.3.5a blizz changed how emblems dropped it should give the player the emblem not have it on its body.
+	 
+// Bronjahm
+#define CN_BRONJAHM 36497
+#define MAGICBANE 68793
+#define CORRUPTSOUL	68839
+#define PHASE_1	36814
+
+class BRONJAHMAI : public MoonScriptBossAI
+{
+		MOONSCRIPT_FACTORY_FUNCTION(BRONJAHMAI, MoonScriptBossAI);
+		BRONJAHMAI(Creature* pCreature) : MoonScriptBossAI(pCreature)
+		{
+			AddSpell(MAGICBANE, Target_Current, 35, 0, 5);
+			AddSpell(CORRUPTSOUL, Target_RandomPlayer, 20, 0, 15, 5, 40, true, "I will sever your soul from your body!", Text_Yell, 16595);
+			AddPhaseSpell(2, AddSpell(68872, Target_Self, 100, 4, 0));
+			AddPhaseSpell(2, AddSpell(70043, Target_Current, 100, 1, 0));
+			AddEmote(Event_OnCombatStart, "Finally... a captive audience!", Text_Yell, 16595);
+			AddEmote(Event_OnTargetDied, "Fodder for the engine!", Text_Yell, 16596);
+			AddEmote(Event_OnTargetDied, "Another soul to strengthen the host!", Text_Yell, 16597);
+
+		};
+
+		void OnDied(Unit* mKiller)
+		{
+			_unit->PlaySoundToSet(16598);
+			ParentClass::OnDied(mKiller);
+		};
+
+		void AIUpdate()
+		{
+			if(GetPhase() == 1)
+			{
+				if(_unit->GetHealthPct() <= 30 )
+				{
+				Emote("The vortex of the harvested calls to you!");
+				_unit->PlaySoundToSet(16599);
+				_unit->Root();
+					SetPhase(2);
+				}
+			ParentClass::AIUpdate();
+		};
+};
+};
+	mgr->register_creature_script(CN_BRONJAHM, &BRONJAHMAI::Create);
+
  }
  
