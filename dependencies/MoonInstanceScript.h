42a47b040d253ede89aee68ac0c9e7ed08bfbbdb
sanctum32 <andriuspel@gmail.com>
2012-05-30 04:31:01 +0300

    fixed MoonInstanceScript + tabs cleanup

--- dependencies/MoonInstanceScript.h
+++ dependencies/MoonInstanceScript.h
@@ -85,100 +85,100 @@ class MoonInstanceScript : public InstanceScript
 		virtual ~MoonInstanceScript();
 
 		// Creature
-		Creature*					GetCreatureBySqlId(uint32 pSqlId);
-		Creature*					GetCreatureByGuid(uint32 pGuid);
-		Creature*					FindClosestCreatureOnMap(uint32 pEntry, float pX, float pY, float pZ);
-		Creature*					SpawnCreature(uint32 pEntry, float pX, float pY, float pZ, float pO);
-		Creature*					SpawnCreature(uint32 pEntry, float pX, float pY, float pZ, float pO, uint32 pFactionId);
-		CreatureSet					FindCreaturesOnMap(uint32 pEntry);
+		Creature*			GetCreatureBySqlId(uint32 pSqlId);
+		Creature*			GetCreatureByGuid(uint32 pGuid);
+		Creature*			FindClosestCreatureOnMap(uint32 pEntry, float pX, float pY, float pZ);
+		Creature*			SpawnCreature(uint32 pEntry, float pX, float pY, float pZ, float pO);
+		Creature*			SpawnCreature(uint32 pEntry, float pX, float pY, float pZ, float pO, uint32 pFactionId);
+		CreatureSet			FindCreaturesOnMap(uint32 pEntry);
 
 		// GameObject
-		GameObject*					GetGameObjectBySqlId(uint32 pSqlId);
-		GameObject*					GetGameObjectByGuid(uint32 pGuid);
-		GameObject*					FindClosestGameObjectOnMap(uint32 pEntry, float pX, float pY, float pZ);
-		GameObject*					SpawnGameObject(uint32 pEntry, float pX, float pY, float pZ, float pO);
-		GameObjectSet				FindGameObjectsOnMap(uint32 pEntry);
-		void						AddGameObjectStateByEntry(uint32 pEntry, GO_STATE pState, bool pUseQuery = false);
-		void						AddGameObjectStateById(uint32 pId, GO_STATE pState);
-		void						AddLeverDoorState(GameObject * pLever, uint32 pDoorEntry);
-		void						AddLeverDoorState(GameObject * pLever, GameObject * pDoor);
-		void						AddLeverDoorStateByGuid(GameObject * pLever, uint32 pDoorGuid);
+		GameObject*			GetGameObjectBySqlId(uint32 pSqlId);
+		GameObject*			GetGameObjectByGuid(uint32 pGuid);
+		GameObject*			FindClosestGameObjectOnMap(uint32 pEntry, float pX, float pY, float pZ);
+		GameObject*			SpawnGameObject(uint32 pEntry, float pX, float pY, float pZ, float pO);
+		GameObjectSet		FindGameObjectsOnMap(uint32 pEntry);
+		void				AddGameObjectStateByEntry(uint32 pEntry, GO_STATE pState, bool pUseQuery = false);
+		void				AddGameObjectStateById(uint32 pId, GO_STATE pState);
+		void				AddLeverDoorState(GameObject * pLever, uint32 pDoorEntry);
+		void				AddLeverDoorState(GameObject * pLever, GameObject * pDoor);
+		void				AddLeverDoorStateByGuid(GameObject * pLever, uint32 pDoorGuid);
 
 		// Distance calculation
-		float						GetRangeToObject(Object* pObjectA, Object* pObjectB);
-		float						GetRangeToObject(Object* pObject, float pX, float pY, float pZ);
-		float						GetRangeToObject(float pX1, float pY1, float pZ1, float pX2, float pY2, float pZ2);
+		float				GetRangeToObject(Object* pObjectA, Object* pObjectB);
+		float				GetRangeToObject(Object* pObject, float pX, float pY, float pZ);
+		float				GetRangeToObject(float pX1, float pY1, float pZ1, float pX2, float pY2, float pZ2);
 
 		// Player and instance - reimplementation for easier calling
-		bool						HasPlayers();
-		size_t						GetPlayerCount();
-		Player*						GetPlayerByGuid(uint32 pGuid);
-		bool						IsCombatInProgress();
+		bool				HasPlayers();
+		size_t				GetPlayerCount();
+		Player*				GetPlayerByGuid(uint32 pGuid);
+		bool				IsCombatInProgress();
 
 		// Timers - reimplementation from MoonScriptCreatureAI
-		int32						AddTimer(int32 pDurationMillisec);
-		int32						GetTimer(int32 pTimerId);
-		void						RemoveTimer(int32 & pTimerId);
-		void						ResetTimer(int32 pTimerId, int32 pDurationMillisec);
-		bool						IsTimerFinished(int32 pTimerId);
-		void						CancelAllTimers();
+		int32				AddTimer(int32 pDurationMillisec);
+		int32				GetTimer(int32 pTimerId);
+		void				RemoveTimer(int32 & pTimerId);
+		void				ResetTimer(int32 pTimerId, int32 pDurationMillisec);
+		bool				IsTimerFinished(int32 pTimerId);
+		void				CancelAllTimers();
 
 		// Update Event
-		void						RegisterScriptUpdateEvent();
-		void						SetUpdateEventFreq(uint32 pUpdateFreq);
-		uint32						GetUpdateEventFreq();
+		void				RegisterScriptUpdateEvent();
+		void				SetUpdateEventFreq(uint32 pUpdateFreq);
+		uint32				GetUpdateEventFreq();
 
 		// Cells
-		void						SetCellForcedStates(float pMinX, float pMaxX, float pMinY, float pMaxY, bool pActivate = true);
+		void				SetCellForcedStates(float pMinX, float pMaxX, float pMinY, float pMaxY, bool pActivate = true);
 
 		//instance difficulty
-		bool						IsHeroic();
-		bool						Is25ManRaid();
-		uint8						GetInstanceMode();
+		bool				IsHeroic();
+		bool				Is25ManRaid();
+		uint8				GetInstanceMode();
 
 		// Player
-		virtual void				OnPlayerDeath(Player* pVictim, Unit* pKiller);
+		void				OnPlayerDeath(Player* pVictim, Unit* pKiller) override;
 
 		// Area and AreaTrigger
-		virtual void				OnPlayerEnter(Player* pPlayer);
-		virtual void				OnAreaTrigger(Player* pPlayer, uint32 pAreaId);
-		virtual void				OnZoneChange(Player* pPlayer, uint32 pNewZone, uint32 pOldZone);
+		void				OnPlayerEnter(Player* pPlayer) override;
+		void				OnAreaTrigger(Player* pPlayer, uint32 pAreaId) override;
+		void				OnZoneChange(Player* pPlayer, uint32 pNewZone, uint32 pOldZone) override;
 
 		// Data get / set - idea taken from ScriptDev2
-		virtual void				SetInstanceData(uint32 pType, uint32 pIndex, uint32 pData);
-		virtual void				SetInstanceData(uint32 pIndex, uint32 pData);
-		virtual uint32				GetInstanceData(uint32 pType, uint32 pIndex);
-		virtual uint32				GetInstanceData(uint32 pIndex);
+		void				SetInstanceData(uint32 pType, uint32 pIndex, uint32 pData) override;
+		void				SetInstanceData(uint32 pIndex, uint32 pData) override;
+		uint32				GetInstanceData(uint32 pType, uint32 pIndex) override;
+		uint32				GetInstanceData(uint32 pIndex) override;
 
 		// Creature / GameObject
-		virtual void				OnCreatureDeath(Creature* pVictim, Unit* pKiller);
-		virtual void				OnCreaturePushToWorld(Creature* pCreature);
-		virtual void				OnGameObjectActivate(GameObject* pGameObject, Player* pPlayer);
-		virtual void				OnGameObjectPushToWorld(GameObject* pGameObject);
+		void				OnCreatureDeath(Creature* pVictim, Unit* pKiller) override;
+		void				OnCreaturePushToWorld(Creature* pCreature) override;
+		void				OnGameObjectActivate(GameObject* pGameObject, Player* pPlayer) override;
+		void				OnGameObjectPushToWorld(GameObject* pGameObject) override;
 
 		// Reimplemented events
-		virtual GameObject*			GetObjectForOpenLock(Player* pCaster, Spell* pSpell, SpellEntry* pSpellEntry);
-		virtual void				SetLockOptions(uint32 pEntryId, GameObject* pGameObject);
-		virtual uint32				GetRespawnTimeForCreature(uint32 pEntryId, Creature* pCreature);
-		void						OnLoad();
-		void						UpdateEvent();
-		void						Destroy();
+		GameObject*			GetObjectForOpenLock(Player* pCaster, Spell* pSpell, SpellEntry* pSpellEntry) override;
+		void				SetLockOptions(uint32 pEntryId, GameObject* pGameObject) override;
+		uint32				GetRespawnTimeForCreature(uint32 pEntryId, Creature* pCreature) override;
+		void				OnLoad();
+		void				UpdateEvent();
+		void				Destroy();
 
 	protected:
 
 		// Encounter generators
-		void						BuildEncounterMap();
-		void						BuildEncounterMapWithEntries(IdVector pEntries);
-		void						BuildEncounterMapWithIds(IdVector pIds);
-		IdVector					BuildIdVector(uint32 pCount, ...);
-		IdSet						BuildIdSet(uint32 pCount, ...);
+		void				BuildEncounterMap();
+		void				BuildEncounterMapWithEntries(IdVector pEntries);
+		void				BuildEncounterMapWithIds(IdVector pIds);
+		IdVector			BuildIdVector(uint32 pCount, ...);
+		IdSet				BuildIdSet(uint32 pCount, ...);
 
-		EncounterMap				mEncounters;
-		WorldStateMap				mWorldStates;
-		GameObjectEntryMap			mGameObjects;
-		uint32						mUpdateFrequency;
-		TimerArray					mTimers;
-		int32						mTimerIdCounter;
+		EncounterMap		mEncounters;
+		WorldStateMap		mWorldStates;
+		GameObjectEntryMap	mGameObjects;
+		uint32				mUpdateFrequency;
+		TimerArray			mTimers;
+		int32				mTimerIdCounter;
 };
 
 #endif /* _INSTANCE_SCRIPT_BASE_H_ */
