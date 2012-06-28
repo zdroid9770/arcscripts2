9752b137cc7942ef92a15d8a60703703be23427a
sanctum32 <andriuspel@gmail.com>
2012-06-28 20:55:37 +0300

    implemented NPC 6329 script

--- scripts/InstanceScripts/Instance_Gnomeregan.cpp
+++ scripts/InstanceScripts/Instance_Gnomeregan.cpp
@@ -18,6 +18,40 @@
 
 #include "Setup.h"
 
+/****************************************/
+/********************************TRASHES*/
+/****************************************/
+class IrradiatedPillagerAI : public MoonScriptCreatureAI
+{
+	public:
+		MOONSCRIPT_FACTORY_FUNCTION(IrradiatedPillagerAI, MoonScriptCreatureAI);
+		IrradiatedPillagerAI(Creature* pCreature) : MoonScriptCreatureAI(pCreature)
+		{
+			AddSpell(9771, Target_Current, 100, 3, rand()%5 + 3);
+			AddSpell(9770, Target_self, 100, 0, 4);
+		}
+
+		void AIUpdate()
+		{
+			//casts fenzy at 50% health
+			if(_unit->GetHealthPch() <= 50 && !_unit->HasAura(8269))
+			{
+				_unit->CastSpell(_unit, 8269, true);
+				Emote("Your bones will break under my boot, $R!", Text_Yell, 0);
+			}
+			ParentClass::AIUpdate();
+		}
+		
+		void OnDied(Unit* mKiller)
+		{
+			Emote("%s blood sprays into the air!", Text_Emote, 0);
+			if(!_unit->HasAura(9798))
+				_unit->CastSpell(_unit, 9798, true);
+			ParentClass::OnDied(mKiller);
+		}
+};
+
 void SetupGnomeregan(ScriptMgr* mgr)
 {
+	mgr->register_creature_script(6329, &IrradiatedPillagerAI::Create);
 }
